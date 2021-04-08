215e4b872d6f151fa07f89c70165b02a40c4b415
muhammadsemeer <muhammadsemeerali@gmail.com>
2021-04-08 17:49:15 +0530

    added top bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import TopBar from "./components/TopBar/TopBar";
 
 function App() {
   return (
     <>
       <Router>
+        <TopBar />
         <Switch>
           <Route exact path="/">
             <Home />
---
+++ src/components/TopBar/TopBar.js
@@ -0,0 +1,11 @@
+const TopBar = () => {
+  const style = {
+    width: "100%",
+    height: "50px",
+    background: "#fff",
+    boxShadow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
+  };
+  return <header style={style}></header>;
+};
+
+export default TopBar;
