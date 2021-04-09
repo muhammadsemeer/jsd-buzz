57f1037056de806a31bce973fe8dbf6352a9ade5
muhammadsemeer <muhammadsemeerali@gmail.com>
2021-04-09 21:51:08 +0530

    used authContext

--- src/App.js
+++ src/App.js
@@ -2,19 +2,22 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import TopBar from "./components/TopBar/TopBar";
 import axios from "axios";
+import AuthContextProvider from "./contexts/authContext";
 axios.defaults.baseURL = process.env.REACT_APP_API_PATH;
 
 function App() {
   return (
     <>
-      <Router>
-        <TopBar />
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </Router>
+      <AuthContextProvider>
+        <Router>
+          <TopBar />
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </Router>
+      </AuthContextProvider>
     </>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,9 @@ import Quiz from "../components/Quiz/Quiz";
 import QuizCode from "../components/QuizCode/QuizCode";
 import SocialCards from "../components/SocialCards/SocialCards";
 import axios from "axios";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
+import { authContext } from "../contexts/authContext";
+import UserPop from "../components/UserPop/UserPop";
 
 const Home = () => {
   const [quiz, setQuiz] = useState([]);
@@ -10,11 +12,13 @@ const Home = () => {
   useEffect(() => {
     axios.get("/quiz").then((response) => {
       setQuiz(response.data);
-      setLoad(true)
+      setLoad(true);
     });
-  },[]);
+  }, []);
+  const { auth } = useContext(authContext);
   return (
     <>
+    {!auth.user ? <UserPop /> : null}
       <main>
         <aside>
           {load ? <Quiz quiz={quiz[0]} /> : null}
