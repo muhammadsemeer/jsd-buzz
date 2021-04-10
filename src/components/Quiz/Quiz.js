a2337d44221a29420b2239f6646d0917fe8934ac
muhammadsemeer <muhammadsemeerali@gmail.com>
2021-04-10 18:36:15 +0530

    posting score to server and checking the user polled already

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -1,10 +1,14 @@
-import { useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import "./Quiz.css";
 import party from "party-js";
+import axios from "axios";
+import { authContext } from "../../contexts/authContext";
 
 const Quiz = ({ quiz }) => {
-  const { question, answerOptions } = quiz[0];
+  const { auth } = useContext(authContext);
+  const { question, answerOptions, _id } = quiz[0];
   const [answered, setAnswered] = useState(false);
+  const [answer, setAnswer] = useState({});
   const checkAnswer = (event) => {
     setAnswered(true);
     if (answerOptions[event.target.id].isCorrect) {
@@ -24,33 +28,79 @@ const Quiz = ({ quiz }) => {
           "rounded-rectangle",
         ],
       });
-      console.log(true);
+      axios.patch(`/quiz/answer/${_id}`, {
+        answered_by: auth.name,
+        option: event.target.id,
+        isCorrect: true,
+      });
     } else {
       document
         .getElementById(event.target.dataset.label)
         .classList.add("wrong");
-      console.log(false);
+      axios.patch(`/quiz/answer/${_id}`, {
+        answered_by: auth.name,
+        option: event.target.id,
+        isCorrect: false,
+      });
     }
   };
+  useEffect(() => {
+    axios
+      .post("/quiz/check-answered", {
+        user: auth.name,
+      })
+      .then(({ data }) => {
+        if (data) {
+          setAnswered(true);
+          setAnswer(data);
+        }
+      });
+  }, [auth.name]);
+  console.log(answer);
   return (
     <div className="quiz">
       <h2 className="question">{question}</h2>
       <div className="options">
         {answerOptions.map((data, index) => (
           <div key={index} className="option-feild">
-            <input
-              type="radio"
-              name="answer"
-              id={index}
-              data-label={`label${index}`}
-              onClick={checkAnswer}
-              value={data.answerText}
-              hidden
-              disabled={answered ? true : false}
-            />
-            <label id={`label${index}`} htmlFor={index}>
-              {data.answerText}
-            </label>
+            {index === parseInt(answer.option) ? (
+              <>
+                <input
+                  type="radio"
+                  name="answer"
+                  id={index}
+                  data-label={`label${index}`}
+                  onChange={checkAnswer}
+                  value={data.answerText}
+                  checked
+                  hidden
+                  disabled={answered ? true : false}
+                />
+                <label
+                  id={`label${index}`}
+                  className={answer.isCorrect ? "" : "wrong"}
+                  htmlFor={index}
+                >
+                  {data.answerText}
+                </label>
+              </>
+            ) : (
+              <>
+                <input
+                  type="radio"
+                  name="answer"
+                  id={index}
+                  data-label={`label${index}`}
+                  onChange={checkAnswer}
+                  value={data.answerText}
+                  hidden
+                  disabled={answered ? true : false}
+                />
+                <label id={`label${index}`} htmlFor={index}>
+                  {data.answerText}
+                </label>
+              </>
+            )}
           </div>
         ))}
       </div>
