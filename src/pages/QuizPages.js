496046e5293ebce5b196d1262f1220f7c0f6d746
muhammadsemeer <muhammadsemeerali@gmail.com>
2021-04-14 19:05:07 +0530

    add quiz compelted

--- src/pages/QuizPages.js
+++ src/pages/QuizPages.js
@@ -1,21 +1,43 @@
-import { useEffect } from "react";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { useHistory } from "react-router";
 import { useForm } from "../utils/useForm/useForm";
 
-const QuizPages = () => {
+const QuizPages = ({ add }) => {
+  const history = useHistory();
   const [value, handleValue] = useForm({
     question: "",
     questionCode: "",
+    expalanation: "",
+    explanationCode: "",
   });
+  const [answerOptions, setAnswerOptions] = useState([]);
+  const [answerText, setAnswerText] = useState("");
+  const [isCorrect, setIsCorrect] = useState(false);
   useEffect(() => {
     document.querySelector("header").style.display = "none";
     return () => {
-      document.querySelector("header").style.display = "felx";
+      document.querySelector("header").style.display = "flex";
     };
   }, []);
+  const addOption = () => {
+    if (answerOptions.length !== 4) {
+      setAnswerOptions([...answerOptions, { answerText, isCorrect }]);
+      setAnswerText("");
+      setIsCorrect(false);
+    }
+  };
+  const addQuiz = (event) => {
+    event.preventDefault();
+    axios.post("/quiz", { ...value, answerOptions }).then(({ data }) => {
+      console.log(data);
+      history.push("/quiz/today");
+    });
+  };
   return (
     <section className="admin">
       <div className="form">
-        <form>
+        <form onSubmit={addQuiz}>
           <h1>Add Quiz</h1>
           <div className="form-field">
             <label htmlFor="question">Question</label>
@@ -25,6 +47,7 @@ const QuizPages = () => {
               id="question"
               onChange={handleValue}
               value={value.question}
+              required
             />
           </div>
           <div className="form-field">
@@ -34,18 +57,44 @@ const QuizPages = () => {
               id="questionCode"
               onChange={handleValue}
               value={value.questionCode}
+              required
+            ></textarea>
+          </div>
+          <div className="form-field">
+            <label htmlFor="expalanation">Explanation</label>
+            <textarea
+              name="expalanation"
+              id="expalanation"
+              onChange={handleValue}
+              value={value.expalanation}
+            ></textarea>
+          </div>
+          <div className="form-field">
+            <label htmlFor="explanationCode">Explanation Code Snippet</label>
+            <textarea
+              name="explanationCode"
+              id="explanationCode"
+              onChange={handleValue}
+              value={value.explanationCode}
             ></textarea>
           </div>
           <div className="form-field">
             <label htmlFor="answerOption">Options</label>
             <div>
-              <input type="text" name="answerOption" id="answerOption" />
+              <input
+                type="text"
+                name="answerOption"
+                id="answerOption"
+                onChange={(e) => setAnswerText(e.target.value)}
+                value={answerText}
+              />
               <div>
                 <input
                   type="radio"
                   name="isCorrect"
                   id="true"
-                  value="true"
+                  onChange={() => setIsCorrect(true)}
+                  checked={isCorrect ? true : false}
                   hidden
                 />
                 <label htmlFor="true">True</label>
@@ -55,16 +104,31 @@ const QuizPages = () => {
                   type="radio"
                   name="isCorrect"
                   id="false"
-                  value="false"
-                  checked
+                  onChange={() => setIsCorrect(false)}
+                  checked={isCorrect ? false : true}
                   hidden
                 />
                 <label htmlFor="false">False</label>
               </div>
-              <input type="button" value="Add" />
+              <input type="button" value="Add" onClick={addOption} />
             </div>
           </div>
-          <div className="options"></div>
+          <div className="options">
+            {answerOptions.map((value, index) => (
+              <div key={index} className="form-field">
+                <div>
+                  <input type="text" value={value.answerText} readOnly />
+                  <input
+                    type="radio"
+                    checked={value.isCorrect}
+                    disabled
+                    hidden
+                  />
+                  <label>{value.isCorrect.toString()}</label>
+                </div>
+              </div>
+            ))}
+          </div>
           <div className="form-field">
             <input type="submit" value="Add" />
           </div>
